ebf23eb7a3cb0a4a54b60cfaef38e7244bd816ea
Dev Shah <dev.shah@directlinegroup.co.uk>
2020-03-30 09:25:23 +0100

    Remove other days code

--- day04/docker-swarm/createVMS.sh
+++
@@ -1,5 +0,0 @@
-docker-machine create mgr-1 -d google --google-machine-type n1-standard-1 --google-tags myswarm --google-project k8s-training-272213 --google-preemptible
-docker-machine create mgr-2 -d google --google-machine-type n1-standard-1 --google-tags myswarm --google-project k8s-training-272213 --google-preemptible
-docker-machine create mgr-3 -d google --google-machine-type n1-standard-1 --google-tags myswarm --google-project k8s-training-272213 --google-preemptible
-docker-machine create w-1 -d google --google-machine-type n1-standard-1 --google-tags myswarm --google-project k8s-training-272213 --google-preemptible
-docker-machine create w-2 -d google --google-machine-type n1-standard-1 --google-tags myswarm --google-project k8s-training-272213 --google-preemptible
